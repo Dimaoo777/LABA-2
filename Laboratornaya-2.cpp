8c0df8c7b94ea66df3f2103ed7e8db7a3f620d53
Dimaoo777 <dima77740rus@gmail.com>
2021-12-27 22:46:27 +0300

    Test
    Test

---
+++ Laboratornaya-2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    string text;
+    getline(cin, text);
+    string reverse;
+
+    reverse_copy(text.begin(), text.end(), back_inserter(reverse));
+    cout << reverse << endl;
+    return 0;
+}
+// Сложность O(n)
